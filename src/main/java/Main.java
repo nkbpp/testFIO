0a1aa3c32642746041ce40c89df998c26c93ac1a
Alexandr <alexbratchin@gmail.com>
2017-01-11 12:21:10 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("FIO");
+        Map<String, ArrayList<String>> customers = new HashMap<>();
+        //tring [] string = {"89517625373","8445921251"};
+        customers.put("Иван", new ArrayList<>(Arrays.asList("sf", "saf")));
+        //customers.put("Иван", Collections.addAll(new {"da,ad,ad"}));
+
+
+        for(String s : customers.keySet()){
+            System.out.println(s + " имеет");
+            for (String t : customers.get(s)){
+                System.out.println("  " + t);
+            }
+        }
+
+        //System.out.println("FIO");
     }
 }
